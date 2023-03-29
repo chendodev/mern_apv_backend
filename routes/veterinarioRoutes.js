02956089dfe6b314d9fe0b8c9b8bc60b4c3145c2
chendo <chendoec@live.com>
2023-03-29 15:46:53 -0500

    Deployment Proyect MERN FULLSTACK

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { 
+  registrar, 
+  perfil, 
+  confirmar, 
+  autenticar, 
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
+  actualizarPerfil,
+  actualizarPassword 
+} from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// área pública
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+// área privada
+router.get('/perfil', checkAuth, perfil)
+router.put('/perfil/:id', checkAuth, actualizarPerfil)
+router.put('/actualizar-password', checkAuth, actualizarPassword)
+
+export default router;
