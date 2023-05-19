71f069370aae22fb39ca90ef4bfe6cd3b5f379d2
Chendo <92568072+chendodev@users.noreply.github.com>
2023-05-19 18:54:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,9 @@ dotenv.config();
 
 connectDB();
 
-const dominiosPermitidos = [process.env.FRONTEND_URL];
-
 const corsOptions = {
-  origin: function(origin, callback) {
-    if(dominiosPermitidos.indexOf(origin) !== -1) {
-      // El Origen del Request esta permitido
-      callback(null, true)
-    } else {
-      callback(new Error('No permitido por CORS'))
-    }
-  }
-}
+  origin: process.env.FRONTEND_URL
+};
 app.use(cors(corsOptions));
 
 app.use('/api/veterinarios', veterinarioRoutes);
